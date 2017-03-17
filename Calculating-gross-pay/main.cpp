2e31d93a5064a4baf1c4118b01853bb9ec14efe7
AliciaDApplications <boricua21dale@gmail.com>
2017-03-17 00:35:02 -0500

    first commit

--- Calculating-gross-pay/main.cpp
+++ Calculating-gross-pay/main.cpp
@@ -8,8 +8,32 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    
+    //declare constant and variables
+    const int PAY_RATE = 10;
+    int hoursWorked = 0;
+    int grossPay = 0;
+    
+    //enter input items
+    cout <<"Hours worked (0-40): ";
+    cin >> hoursWorked;
+    
+    //calculate and display output
+    if (hoursWorked >= 0 && hoursWorked <= 40)
+    {
+        grossPay = hoursWorked * PAY_RATE;
+        cout << "Gross pay: $" << grossPay << endl;
+    }
+    else
+        cout << "Incorrect number of hours" << endl;
+    //end if
+    
+    
+    
+    
     std::cout << "Hello, World!\n";
     return 0;
 }
